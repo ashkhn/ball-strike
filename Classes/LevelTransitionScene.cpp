41c9fa250a56eed8cb54a4fb64a8b3906eaefa1a
Akash Khan <akashkhan25@gmail.com>
2017-01-03 09:23:14 +0800

    Added level transition scene

---
+++ Classes/LevelTransitionScene.cpp
@@ -0,0 +1,30 @@
+#include "LevelTransitionScene.h"
+
+LevelTransitionScene::LevelTransitionScene(){}
+LevelTransitionScene::~LevelTransitionScene(){}
+
+Scene* LevelTransitionScene::createScene(int reason){
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+	
+	// 'layer' is an autorelease object
+	auto layer = LevelTransitionScene::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+
+}
+
+bool LevelTransitionScene::init(){
+	
+	if(!Layer::init()){
+		return false;
+	}
+
+	return true;
+}
+
+
---
+++ Classes/LevelTransitionScene.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "cocos2d.h"
+#include "network/HttpClient.h"
+#include "ui/CocosGUI.h"
+#include "spine/Json.h"
+
+USING_NS_CC;
+
+class LevelTransitionScene: public Layer {
+	public:
+		LevelTransitionScene();
+		virtual ~LevelTransitionScene();
+		static Scene* createScene(int reason);
+		virtual bool init() override;
+		CREATE_FUNC(LevelTransitionScene);
+		ui::Text* status_label;
+		void getCurrentLevel();
+		void updateCurrentLevel(int new_level_id);
+		void onFetchCurrentLevelCompleted(network::HttpClient *sender, network::HttpResponse *response);
+		void onUpdateCurrentLevelCompleted(network::HttpClient *sender, network::HttpResponse *response);
+
+};
