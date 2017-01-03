1a0d21553544f05de4d4355779c241182d946d8d
Akash Khan <akashkhan25@gmail.com>
2017-01-03 10:58:32 +0800

    Modified parsing code to match api response.
      - Changed base url to point to heroku server
      - Removed dummy bool used to open LoginScene everytime

--- Classes/Constants.cpp
+++ Classes/Constants.cpp
@@ -3,10 +3,12 @@
 const char* Constants::IS_USER_LOGGED_IN = "is_user_logged_in";
 const char* Constants::SAVE_EXISTS = "save_exists";
 const char* Constants::IS_RESUMED = "is_resumed";
-const char* Constants::API_BASE_URL = "http://192.168.100.172:3000/api";
+const char* Constants::API_BASE_URL = "https://ballstrike-api.herokuapp.com/api";
 
 
 // Json keys
+const char* Constants::KEY_USER = "user";
+const char* Constants::KEY_GAMELEVELS = "gamelevels";
 const char* Constants::KEY_AUTH_TOKEN = "auth_token";
 const char* Constants::KEY_USER_EMAIL = "email";
 const char* Constants::KEY_USER_CURRENT_LEVEL = "current_level_id";
