ce4bdbb1592388fe233d63669250f36a861e2f95
Akash Khan <akashkhan25@gmail.com>
2017-01-04 11:11:50 +0800

    Added logout to settings. Removed previous settings

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -14,6 +14,7 @@ class Game : public Layer{
 		virtual bool init() override;
 		Size _screen_size;
 		GameLevel* game_level;
+		int num_enemies_left;
 		CREATE_FUNC(Game);
 		virtual void update(float dt) override;
 		void generateLevel(bool is_resumed);
